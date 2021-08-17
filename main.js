7aa6b3c95441d06d03cc0aec638330c537dcdb4e
Himanshu Agarwal <everythinghimanshu@gmail.com>
2021-08-17 14:35:26 +0530

    Updated stop button code

--- main.js
+++ main.js
@@ -44,8 +44,23 @@ function startTimer(duration, display, t) {
     var timer = duration,
         minutes,
         seconds;
+
+    if (duration == 0) {
+        clearInterval(a);
+        a = setInterval(function () {
+            d = d + 1;
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
+
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            display.textContent = minutes + ":" + seconds;
+        }, 1000);
+        return;
+    }
     var d = 0;
-    
+
     a = setInterval(function () {
         d = d + 1;
         minutes = parseInt(timer / 60, 10);
